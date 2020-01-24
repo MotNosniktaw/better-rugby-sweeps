759f993ee779de499ab13edcb1b18a025897d3d6
tom.watkinson <tom.watkinson@backslash.build>
2020-01-24 10:12:45 +0000

    thing

--- src/components/Background.js
+++ src/components/Background.js
@@ -4,7 +4,7 @@ export default function Background({ children }) {
   return (
     <div
       style={{
-        height: "100%",
+        minHeight: "100vh",
         width: "100%",
         background: "#ddbb44",
         display: "flex",
