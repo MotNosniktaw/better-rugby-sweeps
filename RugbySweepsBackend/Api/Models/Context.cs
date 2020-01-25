8ff65242d4dcee86e9940fe2fffdcb740822da6d
tom.watkinson <tom.watkinson@backslash.build>
2020-01-25 15:07:41 +0000

    added Country route to backend

---
+++ RugbySweepsBackend/Api/Models/Context.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Models
+{
+    public class Context: DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+        }
+
+        public DbSet<Country> Countries { get; set; }
+    }
+}
