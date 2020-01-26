e1f68874bcfdabdae4cfecb5313122df5968edc8
tom.watkinson <tom.watkinson@backslash.build>
2020-01-26 15:25:29 +0000

    added matches and results enum

---
+++ RugbySweepsBackend/Api/Enums/Result.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Enums
+{
+    public enum Result
+    {
+        Home, Away, TBD
+    }
+}
---
+++ RugbySweepsBackend/Api/Models/Match.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using Api.Enums;
+
+namespace Api.Models
+{
+    public class Match
+    {
+        public int Id { get; set; }
+        public int WeekNumber { get; set; }
+        public Country HomeTeam { get; set; }
+        public string HomePlayer { get; set; }
+        public Country AwayTeam { get; set; }
+        public string AwayPlayer { get; set; }
+        public DateTime Kickoff { get; set; }
+        public Result Result { get; set; }
+    }
+}
